a8a92e824358b02f0feedbf45dbb049817bf2236
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-05-09 11:40:58 -0500

    error handling on property get

--- server.js
+++ server.js
@@ -84,14 +84,19 @@ app.get('/property/:id', function(req, res) {
       console.log('It worked!');
       console.log(resp);
     } else {
-      console.log(err)
+      console.log(err);
     }
   });
 
 
   //get what the customer wanted;
   client.query('SELECT ' + propertyTable + '.*, ' + brokerTable + '.sfid AS broker__c_sfid, ' + brokerTable + '.name AS broker__c_name, ' + brokerTable + '.email__c AS broker__c_email__c, ' + brokerTable + '.phone__c AS broker__c_phone__c, ' + brokerTable + '.mobile_phone__c AS broker__c_mobile_phone__c, ' + brokerTable + '.title__c AS broker__c_title__c, ' + brokerTable + '.picture__c AS broker__c_picture__c FROM ' + propertyTable + ' INNER JOIN ' + brokerTable + ' ON ' + propertyTable + '.broker__c = ' + brokerTable + '.sfid WHERE ' + propertyTable + '.sfid = $1', [req.params.id], function(error, data) {
-    res.json(data.rows[0]);
+    if (!error){
+      res.json(data.rows[0]);
+    } else {
+      console.log(error);
+      res.send('error');
+    }
   });
 
 });
@@ -107,10 +112,6 @@ app.post('/favorite', function(req, res) {
   client.query('INSERT INTO ' + favoriteTable + ' (property__c) VALUES ($1)', [req.body.property__c], function(error, data) {
     res.json(data);
   });
-});
-
-app.get('/token', function(req, res) {
-  //now call Salesforce API to create the event
 });
 
 app.delete('/favorite/:sfid', function(req, res) {
