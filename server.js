3c4d2af50ab9c42b04625b82c913adad63912f54
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-05-09 10:37:32 -0500

    add nforce, auth from server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var nforce = require('nforce');
 
 var app = express();
 
@@ -23,6 +24,25 @@ client.connect();
 var propertyTable = 'property__c';
 var favoriteTable = 'favorite__c';
 var brokerTable = 'broker__c';
+
+//nforce connection and auth
+
+var org = nforce.createConnection({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.OAUTH_CALLBACK,
+  apiVersion: '40.0'  // optional, defaults to current salesforce API version
+});
+
+var nforceOauth;
+
+org.authenticate({ username: process.env.SF_USERNAME, password: process.env.SF_PASSWORD}, function(err, resp){
+  // store the oauth object for this user
+  if(!err) {
+    nforceOauth = resp;
+  } else { console.log(err);}
+});
+
 
 // setup the demo data if needed
 client.query('SELECT * FROM salesforce.broker__c', function(error, data) {
@@ -67,6 +87,10 @@ app.post('/favorite', function(req, res) {
   client.query('INSERT INTO ' + favoriteTable + ' (property__c) VALUES ($1)', [req.body.property__c], function(error, data) {
     res.json(data);
   });
+});
+
+app.get('/token', function(req, res) {
+  //now call Salesforce API to create the event
 });
 
 app.delete('/favorite/:sfid', function(req, res) {
